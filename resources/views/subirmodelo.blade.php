11a86c8477ad50a4aa5bfe76da496a8dd1332b7f
linux <linux@debian10.4>
2021-05-31 16:23:42 +0200

    deploy 1/7

--- resources/views/subirmodelo.blade.php
+++ resources/views/subirmodelo.blade.php
@@ -59,33 +59,33 @@
                             </div>
                             <div style="display:flex;flex-direction:column;width:45%;margin-right:5%;">
                                 <label for="llantas" style="padding-left:0;font-size:15px;font-weight:bold;" class="col-md-4 col-form-label text-md-left">Llantas</label>
-                                <input style="padding:0 !important; width:100%; margin-bottom:15px; border-right:none; border-top:none; border-left:none; border-bottom:1px solid #C6C6C6;background-color:transparent; outline:none; color:#4E4E4E; caret-color:#939393;font-size:18px;border-radius:0 !important;" type="text" name="llantas" value="" placeholder="Modelo" required>
+                                <input style="padding:0 !important; width:100%; margin-bottom:15px; border-right:none; border-top:none; border-left:none; border-bottom:1px solid #C6C6C6;background-color:transparent; outline:none; color:#4E4E4E; caret-color:#939393;font-size:18px;border-radius:0 !important;" type="text" name="llantas" value="" placeholder='15" o 19"' required>
                             </div>
                         </div>
                         <div class="divcontenedorofertas" style="display:flex;flex-direction:row;width:75%;margin-top:15px;">
                             <div style="display:flex;flex-direction:column;width:45%;margin-right:5%;">   
                                 <label for="capacidad" style="padding-left:0;font-size:15px;font-weight:bold;" class="col-md-4 col-form-label text-md-left">Capacidad</label>
-                                <input style="padding:0 !important; width:100%; margin-bottom:15px; border-right:none; border-top:none; border-left:none; border-bottom:1px solid #C6C6C6;background-color:transparent; outline:none; color:#4E4E4E; caret-color:#939393;font-size:18px;border-radius:0 !important;" type="number" name="capacidad" value="" required>   
+                                <input style="padding:0 !important; width:100%; margin-bottom:15px; border-right:none; border-top:none; border-left:none; border-bottom:1px solid #C6C6C6;background-color:transparent; outline:none; color:#4E4E4E; caret-color:#939393;font-size:18px;border-radius:0 !important;" type="number" name="Deposito" value="" placeholder="100 litros" required>   
                             </div>
                             <div style="display:flex;flex-direction:column;width:45%;margin-right:5%;">
                                 <label for="maletero" style="padding-left:0;font-size:15px;font-weight:bold;" class="col-md-4 col-form-label text-md-left">Maletero</label>
-                                <input style="padding:0 !important; width:100%; margin-bottom:15px; border-right:none; border-top:none; border-left:none; border-bottom:1px solid #C6C6C6;background-color:transparent; outline:none; color:#4E4E4E; caret-color:#939393;font-size:18px;border-radius:0 !important;" type="number" name="maletero" value="" required>
+                                <input style="padding:0 !important; width:100%; margin-bottom:15px; border-right:none; border-top:none; border-left:none; border-bottom:1px solid #C6C6C6;background-color:transparent; outline:none; color:#4E4E4E; caret-color:#939393;font-size:18px;border-radius:0 !important;" type="number" name="maletero" value="" placeholder="100 litros" required>
                             </div>
                         </div>
                         <div class="divcontenedorofertas" style="display:flex;flex-direction:row;width:75%;margin-top:15px;">
                             <div style="display:flex;flex-direction:column;width:45%;margin-right:5%;">   
                                 <label for="description" style="padding-left:0;font-size:15px;font-weight:bold;" class="col-md-4 col-form-label text-md-left">Descripción</label>
-                                <textarea style="padding:0 !important; width:100%; margin-bottom:15px; border-right:none; border-top:none; border-left:none; border-bottom:1px solid #C6C6C6;background-color:transparent; outline:none; color:#4E4E4E; caret-color:#939393;font-size:18px;border-radius:0 !important;" name="description" value=""  required></textarea>
+                                <textarea style="padding:0 !important; width:100%; margin-bottom:15px; border-right:none; border-top:none; border-left:none; border-bottom:1px solid #C6C6C6;background-color:transparent; outline:none; color:#4E4E4E; caret-color:#939393;font-size:18px;border-radius:0 !important;" name="description" value="" placeholder="Descripción 1" required></textarea>
                             </div>
                             <div style="display:flex;flex-direction:column;width:45%;margin-right:5%;">
                                 <label for="description2" style="padding-left:0;font-size:15px;font-weight:bold;" class="col-md-4 col-form-label text-md-left">Descripción 2</label>
-                                <textarea style="padding:0 !important; width:100%; margin-bottom:15px; border-right:none; border-top:none; border-left:none; border-bottom:1px solid #C6C6C6;background-color:transparent; outline:none; color:#4E4E4E; caret-color:#939393;font-size:18px;border-radius:0 !important;" name="description2" value=""  required></textarea>
+                                <textarea style="padding:0 !important; width:100%; margin-bottom:15px; border-right:none; border-top:none; border-left:none; border-bottom:1px solid #C6C6C6;background-color:transparent; outline:none; color:#4E4E4E; caret-color:#939393;font-size:18px;border-radius:0 !important;" name="description2" value=""  placeholder="Descripción 2" required></textarea>
                             </div>
                         </div>
                         <div class="divcontenedorofertas" style="display:flex;flex-direction:row;width:75%;margin-top:15px;">
                             <div style="display:flex;flex-direction:column;width:45%;margin-right:5%;">   
                                 <label for="peso" style="padding-left:0;font-size:15px;font-weight:bold;" class="col-md-4 col-form-label text-md-left">Peso</label>
-                                <input style="padding:0 !important; width:100%; margin-bottom:15px; border-right:none; border-top:none; border-left:none; border-bottom:1px solid #C6C6C6;background-color:transparent; outline:none; color:#4E4E4E; caret-color:#939393;font-size:18px;border-radius:0 !important;" type="number" name="peso" value="" required>   
+                                <input style="padding:0 !important; width:100%; margin-bottom:15px; border-right:none; border-top:none; border-left:none; border-bottom:1px solid #C6C6C6;background-color:transparent; outline:none; color:#4E4E4E; caret-color:#939393;font-size:18px;border-radius:0 !important;" type="number" name="peso" value="" placeholder="1000.00 kg" required>   
                             </div>
                         </div>
                         <div class="divcontenedorofertas" style="display:flex;flex-direction:row;width:75%;margin-top:10vh;">
