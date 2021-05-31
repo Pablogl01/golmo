b5f2f0ae431f15df10e5df7e4b82f6a9ff8c534d
linux <linux@debian10.4>
2021-05-31 16:56:43 +0200

    deploy 1/10

--- config/database.php
+++ config/database.php
@@ -65,13 +65,13 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
+            'url' => env(),
             /*'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),*/
-            'charset' => 'utf8mb4',
+            'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
